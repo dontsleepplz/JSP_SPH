62dc33807cf846f30e793a02946d803e71e03db7
g4826@naver.com <g4826@naver.com>
2023-03-12 17:49:19 +0900

    JSP_SPH [YJM] : update Menu

--- JSP_SPH/src/main/java/kr/project/sph/controller/MenuController.java
+++ JSP_SPH/src/main/java/kr/project/sph/controller/MenuController.java
@@ -19,49 +19,15 @@ import kr.project.sph.service.MenuService;
 
 @Controller
 public class MenuController {
-	
-	  /*
-	  @Resource(name="menuService") private MenuService menuService;
-	  
-	  @GetMapping("/index") public ModelAndView
-	  index(@RequestParam(defaultValue="M000000")String mCode, Model model) throws
-	  Exception{ String url = "/common/indexPage"; List<MenuVO> menuList =
-	  menuService.getMainMenuList(); MenuVO menu =
-	  menuService.getMenuByMcode(mCode);
-	  
-	  
-	  model.addAttribute("menuList", menuList); model.addAttribute("menu",menu);
-	  
-	  ModelAndView mnv = new ModelAndView(); mnv.addObject("model", model);
-	  mnv.setViewName(url);
-	  
-	  return mnv; }
-	  
-	  @GetMapping("/subMenu")
-	  
-	  @ResponseBody public ResponseEntity<List<MenuVO>> subMenuJson(String mCode){
-	  ResponseEntity<List<MenuVO>> entity = null; List<MenuVO> subMenu = null;
-	  
-	  try { subMenu = menuService.getSubMenuList(mCode); entity = new
-	  ResponseEntity<List<MenuVO>>(subMenu, HttpStatus.OK); } catch (Exception e) {
-	  entity = new ResponseEntity<List<MenuVO>>(HttpStatus.INTERNAL_SERVER_ERROR);
-	  e.printStackTrace(); }
-	  
-	  return entity; }
-	 */
-	
+
+	@Resource(name = "menuService")
+	private MenuService menuService;
+
 	@GetMapping("/index")
-	public ModelAndView index(@RequestParam(defaultValue = "M000000") String mCode, Model model, String murl) throws Exception {
+	public ModelAndView index(@RequestParam(defaultValue = "M000000") String mCode, Model model) throws Exception {
 		String url = "/common/indexPage";
-		List<MenuVO> menuList = new ArrayList<MenuVO>();
-		MenuVO menu = new MenuVO();
-
-		// URL 스트링에 직접입력
-		menu.setMcode("M000000");
-		menu.setMurl(murl);
-		menu.setMname("test");
-
-		menuList.add(menu);
+		List<MenuVO> menuList = menuService.getMainMenuList();
+		MenuVO menu = menuService.getMenuByMcode(mCode);
 
 		model.addAttribute("menuList", menuList);
 		model.addAttribute("menu", menu);
@@ -74,12 +40,14 @@ public class MenuController {
 	}
 
 	@GetMapping("/subMenu")
+
 	@ResponseBody
 	public ResponseEntity<List<MenuVO>> subMenuJson(String mCode) {
 		ResponseEntity<List<MenuVO>> entity = null;
 		List<MenuVO> subMenu = null;
 
 		try {
+			subMenu = menuService.getSubMenuList(mCode);
 			entity = new ResponseEntity<List<MenuVO>>(subMenu, HttpStatus.OK);
 		} catch (Exception e) {
 			entity = new ResponseEntity<List<MenuVO>>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -89,4 +57,34 @@ public class MenuController {
 		return entity;
 	}
 
+	/*
+	 * @GetMapping("/index") public ModelAndView index(@RequestParam(defaultValue =
+	 * "M000000") String mCode, Model model, String murl) throws Exception { String
+	 * url = "/common/indexPage"; List<MenuVO> menuList = new ArrayList<MenuVO>();
+	 * MenuVO menu = new MenuVO();
+	 * 
+	 * // URL 스트링에 직접입력 menu.setMcode("M000000"); menu.setMurl(murl);
+	 * menu.setMname("test");
+	 * 
+	 * menuList.add(menu);
+	 * 
+	 * model.addAttribute("menuList", menuList); model.addAttribute("menu", menu);
+	 * 
+	 * ModelAndView mnv = new ModelAndView(); mnv.addObject("model", model);
+	 * mnv.setViewName(url);
+	 * 
+	 * return mnv; }
+	 * 
+	 * @GetMapping("/subMenu")
+	 * 
+	 * @ResponseBody public ResponseEntity<List<MenuVO>> subMenuJson(String mCode) {
+	 * ResponseEntity<List<MenuVO>> entity = null; List<MenuVO> subMenu = null;
+	 * 
+	 * try { entity = new ResponseEntity<List<MenuVO>>(subMenu, HttpStatus.OK); }
+	 * catch (Exception e) { entity = new
+	 * ResponseEntity<List<MenuVO>>(HttpStatus.INTERNAL_SERVER_ERROR);
+	 * e.printStackTrace(); }
+	 * 
+	 * return entity; }
+	 */
 }
